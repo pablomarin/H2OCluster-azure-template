76dfa82789daf79d59074d0518cb0f7e0ff7c236
Pablo Marin <pablo.marin@gmail.com>
2016-08-19 01:10:42 -0500

    create download_h2o.sh
    First create

---
+++ download_h2o.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#AWS_SSH_PRIVATE_KEY_FILE=''
+echo $AWS_SSH_PRIVATE_KEY_FILE
+
+set -e
+
+# Adjust based on the build of H2O you want to download.
+h2oBranch=rel-turchin
+
+echo "Fetching latest build number for branch ${h2oBranch}..."
+curl --silent -o latest https://h2o-release.s3.amazonaws.com/h2o/${h2oBranch}/latest
+h2oBuild=`cat latest`
+
+echo "Fetching full version number for build ${h2oBuild}..."
+curl --silent -o project_version https://h2o-release.s3.amazonaws.com/h2o/${h2oBranch}/${h2oBuild}/project_version
+h2oVersion=`cat project_version`
+
+echo "Downloading H2O version ${h2oVersion} ..."
+
+curl --silent -o h2o-${h2oVersion}.zip https://s3.amazonaws.com/h2o-release/h2o/${h2oBranch}/${h2oBuild}/h2o-${h2oVersion}.zip &
+
+echo "Unzipping h2o.jar ..."
+unzip -o h2o-${h2oVersion}.zip 1> /dev/null &
+
+echo "Copying h2o.jar within node ${i}: ${publicDnsName}"
+cp -f h2o-${h2oVersion}/h2o.jar . &
+
+echo Success.
